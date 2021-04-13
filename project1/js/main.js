fa3a7e507f9404a269d4995b23ffd551e7d9ddfb
Your Name <you@example.com>
2021-04-13 09:55:45 +0600

    first commit

---
+++ project1/js/main.js
@@ -0,0 +1,13 @@
+// bx slider activation or initialization
+
+  $(document).ready(function(){
+    $('.slider_area').bxSlider();
+    jQuery('#main_menu').meanmenu({
+      meanMenuContainer: '#mobile_menu' ,
+      meanScreenWidth: 991,
+
+
+
+    });
+    });
+    
